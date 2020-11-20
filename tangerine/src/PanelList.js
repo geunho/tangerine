23d94066de79ac42067b55fd41583be4081f1035
geunho <geunho.dev@gmail.com>
2020-11-20 22:10:28 +0900

    GTM 추가

--- tangerine/src/PanelList.js
+++ tangerine/src/PanelList.js
@@ -4,7 +4,7 @@ import links from './links';
 const PanelList = () => (
     <div className="Panel-list">
         {links.map((item, idx) => (
-          <a href={item.uri} key={idx}>
+          <a href={item.uri} key={idx} className="click" id={item.id}>
             <div className="Panel">
                 <h3>{item.name}</h3>
             </div>
